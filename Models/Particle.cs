4b6dc48ea3483e523c042455e1197ee1e3799370
Miraj <mirajiik@yandex.ru>
2023-01-12 02:53:13 +0500

    Add command interface, implemented particle lifetime, refactoring

---
+++ Models/Particle.cs
@@ -0,0 +1,44 @@
+﻿using SaveScreenParticles.ViewModels;
+using SaveScreenParticles.ViewModels.Base;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SaveScreenParticles.Models
+{
+    internal class Particle : ViewModel
+    {
+        private int _X;
+        public int X { get => _X; set => Set(ref _X, value); }
+
+        private int _Y;
+        public int Y { get => _Y; set => Set(ref _Y, value); }
+        public Duration LifeTime { get; set; }
+        public Duration MoveTime { get; private set; }
+        private DispatcherTimer _TimerOffset;
+        public DispatcherTimer TimerOffset { get => _TimerOffset;}
+
+        public Particle(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Particle(int x, int y, TimeSpan lifeTime) : this(x, y)
+        {
+            LifeTime = new Duration(lifeTime);
+            MoveTime = new Duration(lifeTime * 2);
+            _TimerOffset = new DispatcherTimer();
+            _TimerOffset.Tick += new EventHandler(OffsetPosition);
+            _TimerOffset.Interval = lifeTime * 2;
+        }
+        private void OffsetPosition(object o, EventArgs e)
+        {
+            var (x, y) = MainWindowViewModel.GenerateRandomPosition();
+            if (this is Particle)
+            {
+                var currentParticle = this;
+                currentParticle.X = x;
+                currentParticle.Y = y;
+            }
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using SaveScreenParticles.Commands;
+using SaveScreenParticles.Models;
+using SaveScreenParticles.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace SaveScreenParticles.ViewModels
+{
+    internal partial class MainWindowViewModel : ViewModel
+    {
+        /*private IEnumerable<Particle> _Particles;
+        public IEnumerable<Particle> Particles { get => _Particles; set => Set(ref _Particles, value); }*/
+        #region Свойства
+        /// <summary>Коллекция частиц</summary>
+        public ObservableCollection<Particle> Particles { get; set; } = new ObservableCollection<Particle>();
+        /// <summary>Фоновая картинка</summary>
+        public BitmapImage Picture { get; set; } = FindImage();
+        /// <summary>Количество частиц</summary>
+        private const int n = 50;
+        private const int width = 900;
+        private const int height = 500;
+        #endregion
+
+        #region Команды
+        #region Запуск таймеров жизни частиц
+        public ICommand StartTimersCommand { get; }
+        private void OnStartTimersCommandExecuted(object p)
+        {
+            foreach (var item in Particles)
+            {
+                item.TimerOffset.Start();
+            }
+        }
+        private bool CanStartTimersCommandExecute(object p) => true;
+        #endregion
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region Инициализация комманд
+            StartTimersCommand = new LambdaCommand(OnStartTimersCommandExecuted, CanStartTimersCommandExecute);
+            #endregion
+            //List<Particle> listParticle = new List<Particle>(n);
+            for (int i = 0; i < n; i++)
+            {
+                var (x, y, lifeTime) = GenerateRandom();
+                Particles.Add(new Particle(x, y, TimeSpan.FromSeconds(lifeTime)));
+            }
+            //Particles = listParticle;
+            //listParticle.Clear();
+        }
+
+        static private BitmapImage FindImage()
+        {
+            var fileNames = Directory.EnumerateFiles("../../../img", "*.*")
+                .Where(f => f.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase)
+                || f.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase)
+                || f.EndsWith(".png", StringComparison.OrdinalIgnoreCase)
+                || f.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase));
+            if (fileNames.Count() == 0)
+            {
+                MessageBox.Show("Ошибка! Не найдено изображение в папке img!");
+                Application.Current.Shutdown();
+                return null;
+            }
+
+            return new BitmapImage(new Uri(fileNames.First(), UriKind.Relative));
+        }
+
+        static public (int, int) GenerateRandomPosition()
+        {
+            Random rnd = new Random();
+            return (rnd.Next(-width, width), rnd.Next(-height, height));
+        }
+        static public (int, int, int) GenerateRandom()
+        {
+            Random rnd = new Random();
+            return (rnd.Next(-width, width), rnd.Next(-height, height), rnd.Next(2,10));
+        }
+    }
+}
